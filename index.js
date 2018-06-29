5ce526fd475c5f9b9e1b4a9031a5929a7b35f3ff
cayetanomarco@yahoo.com <cayetanomarco@yahoo.com>
2018-06-29 21:27:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ function dec(n){
 function makeInt(n){
   return parseInt(n);
 }
-functin makeInt(n){
-  re
+function makeInt(n){
+  return parseInt(n, 10);
+}
+function preserveDecimal(n){
+  return parseFloat(n);
 }
